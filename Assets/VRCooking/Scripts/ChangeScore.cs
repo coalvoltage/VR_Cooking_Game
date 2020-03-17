f28cde361af92efb99f6aed62c8309567a440729
Jeffocube <30770176+Jeffocube@users.noreply.github.com>
2020-03-17 05:16:56 -0700

    Score and Time
    Finished some code but didn't apply score to any objects yet.

---
+++ Assets/VRCooking/Scripts/ChangeScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScore : MonoBehaviour
+{
+	public string gameO1;
+	public string gameO2;
+	public string gameO3;
+	public string theText;
+	public int score;
+	public GameObject g1, g2, g3;
+    // Start is called before the first frame update
+    void Start()
+    {
+		g1 = GameObject.Find(gameO1);
+		g2 = GameObject.Find(gameO2);
+		g3 = GameObject.Find(gameO3);
+		score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMesh t = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
+        score = g1.GetComponent<CFood>().completed + g2.GetComponent<CFood>().completed + g3.GetComponent<CFood>().completed;
+		t.text = score.ToString();
+    }
+}
