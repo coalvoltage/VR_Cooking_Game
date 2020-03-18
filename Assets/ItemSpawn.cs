bd18aadc4a08fc929c8721f0f322a774a81e30a4
jchoi071 <jchoi071@ucr.edu>
2020-03-17 20:39:11 -0700

    Added Item Spawner script
    Attach script to item to spawn. New box collider on game object "kitchen_stand (1)".

---
+++ Assets/ItemSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+	public bool newItem;
+	public Vector3 position;
+	public Quaternion rotation;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        newItem = true;
+		position = gameObject.transform.position;
+		rotation = gameObject.transform.rotation;
+    }
+	
+	void OnCollisionExit(Collision collision)
+	{
+		if (newItem && (collision.gameObject.name == "kitchen_stand (1)"))
+		{
+			Instantiate(gameObject, position, rotation);
+			newItem = false;
+		}
+	}
+}
